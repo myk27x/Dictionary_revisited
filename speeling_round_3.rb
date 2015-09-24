b20bace1ca8ac6eaa0f162ef082b7b6ec946b1b3
Michael Reed <myk27x@yahoo.com>
2015-09-24 01:27:03 -0400

    searh and add completed. could not make words display to frontend page but show in terminal

--- speeling_round_3.rb
+++ speeling_round_3.rb
@@ -1,3 +1,15 @@
+require 'active_record'
+require 'sqlite3'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: File.dirname(__FILE__) + "/definitions.db"
+  )
+
+class Definition < ActiveRecord::Base
+end
+
+
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'erb'
@@ -6,7 +18,18 @@ set :port, 3027
 
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
+def return_dict
+  Definition.all.each do |value|
+    @words = value.word
+    @meanings = value.meaning
+    puts "#{@words} = #{@meanings}"
+  end
+end
+
 get "/" do
+
+  # return "words = meanings" from definitions table
+
   erb :home
 end
 
@@ -15,9 +38,14 @@ get "/add" do
 end
 
 post "/save" do
+  Definition.create(word: params["word"], meaning: params["meaning"])
+
   redirect '/'
 end
 
 get "/search" do
+
+  definition = Definition.find_by(word: params[:search])
+  @definition = "#{definition.word} = #{definition.meaning}"
   erb :search
 end
